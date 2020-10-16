cc6764441d58c84c498d1ce28a00fc939bd7d8f6
Chantal Desiree <chantaldesiree@outlook.com>
2020-10-16 13:48:37 -0500

    Created seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'poke-api-v2'
+
+Trainer.delete_all
+Pokemon.delete_all
+
+#20 regions (0-19)
+
+50.times do
+    trainer = Trainer.create(
+        name: Faker::Name.name, 
+        region_id: rand(0..7), 
+        trainer_pokemon: PokeApi.get(pokemon: rand(1..450)).name.capitalize())
+    #puts "***********************************"
+    #puts trainer.name
+    #puts trainer.region_id
+    #puts trainer.trainer_pokemon.capitalize()
+end
+
+puts "Created #{Trainer.count} trainers."
+
+450.times do |i|
+    pokemon = PokeApi.get(pokemon: pokemons = PokeApi.get(pokemon: i+1).name)
+
+    pokemon = Pokemon.create(
+        name: pokemon.name, 
+        pokemon_type: pokemon.types[0].type.name, 
+        region_id: rand(0..7))
+    #puts pokemon.name
+    #puts pokemon.pokemon_type
+    #puts pokemon.regi
+    #puts "*************************"
+end
+
+#Gets a specific pokemon by ID.
+#pokemons = PokeApi.get(pokemon: 1)
+#puts pokemons.name
+
+puts "Created #{Pokemon.count} pokemon."
+
+
