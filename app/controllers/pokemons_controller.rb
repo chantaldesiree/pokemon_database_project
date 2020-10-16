7e083e5907b41caf08e0cf10063598e21d400b52
Chantal Desiree <chantaldesiree@outlook.com>
2020-10-16 15:49:55 -0500

    4.1 - Added a search for Pokemon.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,4 +6,9 @@ class PokemonsController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
   end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @pokemons = Pokemon.where("name LIKE ?", wildcard_search)
+  end
 end
