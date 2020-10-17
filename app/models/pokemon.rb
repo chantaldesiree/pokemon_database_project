b8b819663b021b23901ea3cae6d0ca76b525fa89
Chantal Desiree <chantaldesiree@outlook.com>
2020-10-16 22:44:29 -0500

    1.3 Added TrainerPokemon model.

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,4 @@
 class Pokemon < ApplicationRecord
+    has_many :trainers
     validates :name, :region_id, :pokemon_type, presence: true
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,4 @@
 class Trainer < ApplicationRecord
+    has_many :pokemons
     validates :name, :region_id, :trainer_pokemon, presence: true
 end
