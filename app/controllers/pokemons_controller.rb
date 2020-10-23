455c601e9c02b40c85d61db72c7ce87a18b5e5c3
Chantal Desiree <chantaldesiree@outlook.com>
2020-10-23 02:55:16 -0500

    Created Home model.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,6 +5,15 @@ class PokemonsController < ApplicationController
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    @region = {
+      "0"  => "kanto",
+      "1"  => "johto",
+      "2"  => "hoenn",
+      "3"  => "sinnoh",
+      "4"  => "unova",
+      "5"  => "kalos",
+      "6"  => "alola",
+      "7"  => "galar" }
   end
 
   def search
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,9 +1,29 @@
 class TrainersController < ApplicationController
+
+
   def index
     @trainers = Trainer.all
+    @region = {
+      "0"  => "kanto",
+      "1"  => "johto",
+      "2"  => "hoenn",
+      "3"  => "sinnoh",
+      "4"  => "unova",
+      "5"  => "kalos",
+      "6"  => "alola",
+      "7"  => "galar" }
   end
 
   def show
     @trainer = Trainer.find(params[:id])
+    @region = {
+      "0"  => "kanto",
+      "1"  => "johto",
+      "2"  => "hoenn",
+      "3"  => "sinnoh",
+      "4"  => "unova",
+      "5"  => "kalos",
+      "6"  => "alola",
+      "7"  => "galar" }
   end
 end
