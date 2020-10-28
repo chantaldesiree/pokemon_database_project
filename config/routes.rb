e51748ff9a9663f4775cf5c1027ca9c07a1b7c33
Chantal Desiree <chantaldesiree@outlook.com>
2020-10-28 13:29:03 -0500

    Added an about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :about, only: %i[index]
   resources :trainers, only: %i[index show]
   resources :pokemons, only: %i[index show] do
     collection do
